86552bbe7c6a863e24fc104f81d633bbcbdae7c9
Liviu Rau <liviu_aurelian@users.sf.net>
2003-07-29 14:11:40 +0000

    *** empty log message ***
    git-svn-id: https://jgrapht.svn.sourceforge.net/svnroot/jgrapht/trunk@41 4a7320f3-3611-0410-a4d0-ef7ff04b8906

---
+++ src/org/_3pq/jgrapht/alg/test/SimpleStackTest.java
@@ -0,0 +1,43 @@
+/*
+ * Created on Jul 29, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package org._3pq.jgrapht.alg.test;
+
+import org._3pq.jgrapht.alg.SimpleStack;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class SimpleStackTest extends TestCase {
+
+	public void testIsEmpty() {
+		SimpleStack ss = new SimpleStack();
+		assertTrue(ss.isEmpty());
+	}
+
+	public void testAdd() {
+		SimpleStack ss = new SimpleStack();
+		ss.add(new Object());
+		assertFalse(ss.isEmpty());
+	}
+
+	public void testRemove() {
+		SimpleStack ss = new SimpleStack();
+		Object o = new Object();
+		ss.add(o);
+		ss.add("string");
+		assertEquals("string", ss.remove());
+		assertFalse(ss.isEmpty());
+		assertEquals(o, ss.remove());
+		assertTrue(ss.isEmpty());
+	}
+
+}
