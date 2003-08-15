ed52f24c20945754206ab8f135675f8d3a38d1dd
Barak Naveh <barak_naveh@users.sf.net>
2003-08-15 15:10:28 +0000

    removed unused imports and formatted with jalopy
    git-svn-id: https://jgrapht.svn.sourceforge.net/svnroot/jgrapht/trunk@169 4a7320f3-3611-0410-a4d0-ef7ff04b8906

--- src/org/_3pq/jgrapht/graph/AsUndirectedGraph.java
+++ src/org/_3pq/jgrapht/graph/AsUndirectedGraph.java
@@ -24,7 +24,7 @@
 /* ----------------------
  * AsUndirectedGraph.java
  * ----------------------
- * (C) Copyright 2003, by Barak Naveh and Contributors.
+ * (C) Copyright 2003, by John V. Sichi and Contributors.
  *
  * Original Author:  John V. Sichi
  * Contributor(s):   -
@@ -40,36 +40,35 @@ package org._3pq.jgrapht.graph;
 
 import java.io.Serializable;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
-import org._3pq.jgrapht.Edge;
-import org._3pq.jgrapht.Graph;
-import org._3pq.jgrapht.UndirectedGraph;
 import org._3pq.jgrapht.DirectedGraph;
-
+import org._3pq.jgrapht.Edge;
+import org._3pq.jgrapht.UndirectedGraph;
 import org._3pq.jgrapht.edge.UndirectedEdge;
 
 /**
  * An undirected view of the backing directed graph specified in the
  * constructor.  This graph allows modules to apply algorithms designed for
- * undirected graphs to a directed graph by simply ignoring edge direction.  If
+ * undirected graphs to a directed graph by simply ignoring edge direction. If
  * the backing directed graph is an <a
  * href="http://mathworld.wolfram.com/OrientedGraph.html"> oriented graph</a>,
  * then the view will be a simple graph; otherwise, it will be a multigraph.
  * Query operations on this graph "read through" to the backing graph.
  * Attempts to add edges will result in an an
- * <code>UnsupportedOperationException</code>, but vertex addition/removal
- * and edge removal are all supported (and immediately reflected in the backing
+ * <code>UnsupportedOperationException</code>, but vertex addition/removal and
+ * edge removal are all supported (and immediately reflected in the backing
  * graph).
- *
- * <p> Note that edges returned by this graph's accessors are really just the
- * edges of the underlying directed graph.  Since there is no interface
- * distinction between directed and undirected edges, this detail should be
- * irrelevant to algorithms.
+ * 
+ * <p>
+ * Note that edges returned by this graph's accessors are really just the edges
+ * of the underlying directed graph.  Since there is no interface distinction
+ * between directed and undirected edges, this detail should be irrelevant to
+ * algorithms.
+ * </p>
  * 
  * <p>
  * This graph does <i>not</i> pass the hashCode and equals operations through
@@ -82,55 +81,82 @@ import org._3pq.jgrapht.edge.UndirectedEdge;
  *
  * @since Aug 14, 2003
  */
-public class AsUndirectedGraph extends GraphDelegator
-    implements Serializable, UndirectedGraph {
-    
+public class AsUndirectedGraph extends GraphDelegator implements Serializable,
+    UndirectedGraph {
     private static final String NO_EDGE_ADD =
-    "this graph does not support edge addition";
-    
+        "this graph does not support edge addition";
     private static final String UNDIRECTED =
-    "this graph only supports undirected operations";
+        "this graph only supports undirected operations";
 
     /**
      * Constructor for AsUndirectedGraph.
      *
      * @param g the backing directed graph over which an undirected view is to
-     * be created.
+     *        be created.
      */
     public AsUndirectedGraph( DirectedGraph g ) {
         super( g );
     }
 
     /**
-     * @see Graph#getAllEdges(Object, Object)
+     * @see org._3pq.jgrapht.Graph#getAllEdges(Object, Object)
      */
     public List getAllEdges( Object sourceVertex, Object targetVertex ) {
         List forwardList = super.getAllEdges( sourceVertex, targetVertex );
 
-        if ( sourceVertex.equals( targetVertex ) ) {
+        if( sourceVertex.equals( targetVertex ) ) {
             // avoid duplicating loops
             return forwardList;
         }
-        
+
         List reverseList = super.getAllEdges( targetVertex, sourceVertex );
-        List list = new ArrayList(
-            forwardList.size(  ) + reverseList.size(  ) );
+        List list =
+            new ArrayList( forwardList.size(  ) + reverseList.size(  ) );
         list.addAll( forwardList );
         list.addAll( reverseList );
+
         return list;
     }
 
+
     /**
-     * @see Graph#getEdge(Object, Object)
+     * @see org._3pq.jgrapht.Graph#getEdge(Object, Object)
      */
     public Edge getEdge( Object sourceVertex, Object targetVertex ) {
         Edge edge = super.getEdge( sourceVertex, targetVertex );
-        if ( edge != null ) {
+
+        if( edge != null ) {
             return edge;
         }
+
         // try the other direction
         return super.getEdge( targetVertex, sourceVertex );
     }
+
+
+    /**
+     * @see org._3pq.jgrapht.Graph#addAllEdges(Collection)
+     */
+    public boolean addAllEdges( Collection edges ) {
+        throw new UnsupportedOperationException( NO_EDGE_ADD );
+    }
+
+
+    /**
+     * @see org._3pq.jgrapht.Graph#addEdge(Edge)
+     */
+    public boolean addEdge( Edge e ) {
+        throw new UnsupportedOperationException( NO_EDGE_ADD );
+    }
+
+
+    /**
+     * @see org._3pq.jgrapht.Graph#addEdge(Object, Object)
+     */
+    public Edge addEdge( Object sourceVertex, Object targetVertex ) {
+        throw new UnsupportedOperationException( NO_EDGE_ADD );
+    }
+
 
     /**
      * @see UndirectedGraph#degreeOf(Object)
@@ -140,45 +166,6 @@ public class AsUndirectedGraph extends GraphDelegator
         return super.inDegreeOf( vertex ) + super.outDegreeOf( vertex );
     }
 
-    /**
-     * @see AbstractBaseGraph#toString()
-     */
-    public String toString(  ) {
-        // take care to print edges using the undirected convention
-        Collection edgeSet = new ArrayList();
-
-        Iterator iter = edgeSet(  ).iterator(  );
-        while ( iter.hasNext(  ) ) {
-            Edge edge = (Edge) iter.next(  );
-            edgeSet.add(
-                new UndirectedEdge( edge.getSource(  ), edge.getTarget(  ) ) );
-        }
-        
-        return super.toStringFromSets( vertexSet(  ), edgeSet );
-    }
-    
-    // the methods below suppress the default GraphDelegator behavior
-    
-    /**
-     * @see Graph#addAllEdges(Collection)
-     */
-    public boolean addAllEdges( Collection edges ) {
-        throw new UnsupportedOperationException( NO_EDGE_ADD );
-    }
-
-    /**
-     * @see Graph#addEdge(Edge)
-     */
-    public boolean addEdge( Edge e ) {
-        throw new UnsupportedOperationException( NO_EDGE_ADD );
-    }
-
-    /**
-     * @see Graph#addEdge(Object, Object)
-     */
-    public Edge addEdge( Object sourceVertex, Object targetVertex ) {
-        throw new UnsupportedOperationException( NO_EDGE_ADD );
-    }
 
     /**
      * @see DirectedGraph#inDegreeOf(Object)
@@ -209,5 +196,24 @@ public class AsUndirectedGraph extends GraphDelegator
      */
     public List outgoingEdgesOf( Object vertex ) {
         throw new UnsupportedOperationException( UNDIRECTED );
+    }
+
+
+    /**
+     * @see AbstractBaseGraph#toString()
+     */
+    public String toString(  ) {
+        // take care to print edges using the undirected convention
+        Collection edgeSet = new ArrayList(  );
+
+        Iterator   iter = edgeSet(  ).iterator(  );
+
+        while( iter.hasNext(  ) ) {
+            Edge edge = (Edge) iter.next(  );
+            edgeSet.add( new UndirectedEdge( edge.getSource(  ),
+                    edge.getTarget(  ) ) );
+        }
+
+        return super.toStringFromSets( vertexSet(  ), edgeSet );
     }
 }
