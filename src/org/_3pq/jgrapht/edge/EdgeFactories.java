3941b6bef3bf72beb7a100f371afcccaab5f6209
Barak Naveh <barak_naveh@users.sf.net>
2004-03-08 16:26:13 +0000

    Eliminated email from headers.
    git-svn-id: https://jgrapht.svn.sourceforge.net/svnroot/jgrapht/trunk@295 4a7320f3-3611-0410-a4d0-ef7ff04b8906

--- src/org/_3pq/jgrapht/edge/EdgeFactories.java
+++ src/org/_3pq/jgrapht/edge/EdgeFactories.java
@@ -3,9 +3,9 @@
  * ==========================================
  *
  * Project Info:  http://jgrapht.sourceforge.net/
- * Project Lead:  Barak Naveh (barak_naveh@users.sourceforge.net)
+ * Project Lead:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
  *
- * (C) Copyright 2003, by Barak Naveh and Contributors.
+ * (C) Copyright 2003-2004, by Barak Naveh and Contributors.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -35,8 +35,8 @@
  * -------
  * 24-Jul-2003 : Initial revision (BN);
  * 04-Aug-2003 : Renamed from EdgeFactoryFactory & made utility class (BN);
- * 03-Nov-2003 : Made edge factories serializable (BN). 
- * 
+ * 03-Nov-2003 : Made edge factories serializable (BN);
+ *
  */
 package org._3pq.jgrapht.edge;
 
