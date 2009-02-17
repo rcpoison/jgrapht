3860b62b9b82aa02235c74579d1111aab87582d8
Andrew Newell <ajnewell@users.sf.net>
2009-02-17 17:05:00 +0000

    included a line for HamiltonianCycleTest
    git-svn-id: https://jgrapht.svn.sourceforge.net/svnroot/jgrapht/trunk@670 4a7320f3-3611-0410-a4d0-ef7ff04b8906

--- testsrc/org/jgrapht/alg/AllAlgTests.java
+++ testsrc/org/jgrapht/alg/AllAlgTests.java
@@ -85,6 +85,7 @@ public final class AllAlgTests
         suite.addTest(new TestSuite(EdmondsKarpMaximumFlowTest.class));
         suite.addTest(new TestSuite(ChromaticNumberTest.class));
         suite.addTest(new TestSuite(EulerianCircuitTest.class));
+        suite.addTest(new TestSuite(HamiltonianCycleTest.class));
 
         // $JUnit-END$
         return suite;
