8d8a2284a7560dc91f5a4a152e8b94871a8064a6
John V Sichi <perfecthash@users.sf.net>
2009-07-06 02:18:35 +0000

    Some cleanup for GraphUnion and friends.
    git-svn-id: https://jgrapht.svn.sourceforge.net/svnroot/jgrapht/trunk@695 4a7320f3-3611-0410-a4d0-ef7ff04b8906

--- src/org/jgrapht/graph/GraphUnion.java
+++ src/org/jgrapht/graph/GraphUnion.java
@@ -1,3 +1,41 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2009, by Barak Naveh and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* -------------------------
+ * GraphUnion.java
+ * -------------------------
+ * (C) Copyright 2009-2009, by Ilya Razenshteyn
+ *
+ * Original Author:  Ilya Razenshteyn and Contributors.
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ * 02-Feb-2009 : Initial revision (IR);
+ *
+ */
 package org.jgrapht.graph;
 
 import java.io.*;
@@ -247,4 +285,4 @@ public class GraphUnion<V, E, G extends Graph<V, E>>
     }
 }
 
-// End $file.name$
+// End GraphUnion.java
