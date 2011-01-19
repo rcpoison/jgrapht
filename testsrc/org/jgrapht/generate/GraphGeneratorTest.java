9286c258f17b839c955cf7c1d4d3f3531ca48d5a
John V Sichi <perfecthash@users.sf.net>
2011-01-19 03:44:47 +0000

    Add GridGraphGenerator, contributed by Assaf Mizrachi.
    git-svn-id: https://jgrapht.svn.sourceforge.net/svnroot/jgrapht/trunk@736 4a7320f3-3611-0410-a4d0-ef7ff04b8906

--- testsrc/org/jgrapht/generate/GraphGeneratorTest.java
+++ testsrc/org/jgrapht/generate/GraphGeneratorTest.java
@@ -289,7 +289,95 @@ public class GraphGeneratorTest
         assertTrue(map.get(StarGraphGenerator.CENTER_VERTEX) != null);
     }
 
-    // TODO:  testWheelGraphGenerator
+    /**
+     * .
+     */
+    public void testGridGraphGenerator() {
+		int rows = 3;
+		int cols = 4;
+		
+		//the form of these two classes helps debugging
+		class StringVertexFactory implements VertexFactory<String> {
+
+			int index = 1;
+			@Override
+			public String createVertex() {
+				return String.valueOf(index++);
+			}
+			
+		}
+		class StringEdgeFactory implements EdgeFactory<String, String> {
+
+			@Override
+			public String createEdge(String sourceVertex, String targetVertex) {
+				return new String(sourceVertex + '-' + targetVertex);
+			}
+			
+		}
+		
+		GridGraphGenerator<String, String> generator = new GridGraphGenerator<String, String>(rows, cols);
+		Map<String, String> resultMap = new HashMap<String, String>();
+		
+		//validating a directed and undirected graph
+		Graph<String, String> directedGridGraph = new DefaultDirectedGraph<String, String>(new StringEdgeFactory());
+		generator.generateGraph(directedGridGraph, new StringVertexFactory(), resultMap);		
+		validateGridGraphGenerator(rows, cols, directedGridGraph, resultMap);
+		
+		resultMap.clear();
+		Graph<String, String> undirectedGridGraph = new SimpleGraph<String, String>(new StringEdgeFactory());
+		generator.generateGraph(undirectedGridGraph, new StringVertexFactory(), resultMap);		
+		validateGridGraphGenerator(rows, cols, undirectedGridGraph, resultMap);
+	}
+    
+    public void validateGridGraphGenerator(int rows, int cols, Graph<String, String> gridGraph, Map<String, String> resultMap) {
+    	//graph structure validations
+    	int expectedVerticeNum = rows * cols;
+    	assertEquals("number of vertices is wrong (" + gridGraph.vertexSet().size() +
+    			"), should be " + expectedVerticeNum, expectedVerticeNum, gridGraph.vertexSet().size());
+    	int expectedEdgesNum = ((rows - 1) * cols + (cols - 1) * rows) * (gridGraph instanceof UndirectedGraph ? 1 : 2);
+    	assertEquals("number of edges is wrong (" + gridGraph.edgeSet().size()
+    			+ "), should be " + expectedEdgesNum, expectedEdgesNum, gridGraph.edgeSet().size());
+
+    	int cornerVertices = 0, borderVertices = 0, innerVertices = 0, neighborsSize;
+    	int expCornerVertices = 4; 
+    	int expBorderVertices = Math.max((rows - 2) * 2 + (cols - 2) * 2, 0);
+    	int expInnerVertices = Math.max((rows - 2) * (cols - 2), 0);
+    	Set<String> neighbors = new HashSet<String>();
+    	
+    	for (String v : gridGraph.vertexSet()) {
+    		neighbors.clear();
+    		neighbors.addAll(Graphs.neighborListOf(gridGraph, v));
+    		neighborsSize = neighbors.size(); 
+    		assertTrue("vertex with illegal number of neighbors (" + neighborsSize + ").",
+    				neighborsSize == 2 || neighborsSize == 3 || neighborsSize == 4);
+    		if (neighborsSize == 2) {
+    			cornerVertices++;
+    		} else if (neighborsSize == 3) {
+    			borderVertices++;
+    		} else if (neighborsSize == 4) {
+    			innerVertices++;
+    		}				
+    	}
+    	assertEquals("there should be exactly " + expCornerVertices + " corner (with two neighbors) vertices. " 
+    			+ " actual number is " + cornerVertices + ".", expCornerVertices, cornerVertices);
+    	assertEquals("there should be exactly " + expBorderVertices + " border (with three neighbors) vertices. " 
+    			+ " actual number is " + borderVertices + ".", expBorderVertices, borderVertices);
+    	assertEquals("there should be exactly " + expInnerVertices + " inner (with four neighbors) vertices. "
+    			+ " actual number is " + innerVertices + ".", expInnerVertices, innerVertices);
+    	
+    	//result map validations
+    	Set<String> keys = resultMap.keySet();
+    	assertEquals("result map contains should contains exactly 4 corner verices",
+    			4, keys.size());
+    	
+    	for (String key : keys) {
+    		neighbors.clear();
+    		neighbors.addAll(Graphs.neighborListOf(gridGraph, resultMap.get(key)));
+    		neighborsSize = neighbors.size(); 
+    		assertEquals("corner vertex should have exactly 2 neighbors",
+    				2, neighborsSize);
+    	}
+    }
 }
 
 // End GraphGeneratorTest.java
